59b8efbe6a4188fac0bfc47b3b7b9a648794d94d
luisumes16 <123779843+luisumes16@users.noreply.github.com>
2023-05-11 20:42:26 -0600

    Update juego.cpp

--- juego.cpp
+++ juego.cpp
@@ -7,52 +7,58 @@
 #include <iostream>
 using namespace std;
 
-void piramide(int fila1, int fila2,int fila3){
 
+void piramide(int fila1, int fila2, int fila3){
+    cout<<fila1<<" "<<fila2<<" "<<fila3<<endl;
+    int fila = 0;
+    int restante = 0;
+    cout<<"      ";
     for(int i = 0; i<fila1; i++){
-        cout<<"    | ";
-    }
-    cout<<endl;
-    for(int i = 0; i<fila2; i++){
-        cout<<"  | ";
-    }
-    cout<<endl;
-    for(int i = 0; i<fila3; i++){
-        cout<<" | ";
-    }
-    cout<<endl;
+        cout<<" * ";
 
-    int opcion,sustraendo;
-    cout<<"\na que fila desea acceder (1,2,3)\n";
-    cin>>opcion;
-    cout<<"\ncuantos elementos desea retirar\n";
-    cin>>sustraendo;
-    switch(opcion){
+    }
+    cout<<endl;
+    cout<<"   ";
+    for(int i = 0; i<fila2; i++) {
+        cout << " * ";
+    }
+    cout<<endl;
+    cout<<"";
+    for(int i = 0; i<fila3; i++) {
+        cout << " * ";
+    }
+    cout<<endl;
+    //Quitar puntos
+    cout<<"Cuantos va a quitar?"<<endl;
+    cin>>restante;
+    cout<<"De que fila?"<<endl;
+    cin>>fila;
+
+    switch(fila){
         case 1:
-            fila1 = fila1 - sustraendo;
-            piramide(fila1,fila2,fila3);
+            fila1 = fila1 - restante;
+            piramide(fila1, fila2, fila3);
             break;
         case 2:
-            fila2 = fila2 - sustraendo;
-            piramide(fila1,fila2,fila3);
+            fila2 = fila2 - restante;
+            piramide(fila1, fila2, fila3);
             break;
         case 3:
-            fila3 = fila3 - sustraendo;
-            piramide(fila1,fila2,fila3);
+            fila3 = fila3 - restante;
+            piramide(fila1, fila2, fila3);
             break;
-                default: cout<<"Valor incorrecto";
+        default:
+            break;
     }
+
 
 }
 
-int main() {
- 
+int main(){
     int fila1 = 3;
     int fila2 = 5;
     int fila3 = 7;
- 
-    piramide(fila1, fila2,fila3);
 
-
-    return 0;
+    piramide(fila1, fila2, fila3);
 }
+
